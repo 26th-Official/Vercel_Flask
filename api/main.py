6fc1c7334bc88ab3f99dec2f88d314b6a773748d
26th-Official <you@example.com>
2023-08-15 09:10:38 +0530

    Init

---
+++ api/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def Sample():
+    return jsonify({
+        "Status" : "Success",
+        "Text" : "Hello World"
+    }),200
+    
+@app.route("/test")
+def Sample():
+    return jsonify({
+        "Status" : "Success",
+        "Text" : "Fire"
+    }),200
